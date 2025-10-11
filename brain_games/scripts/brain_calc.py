7083da824efc7530122e8be2ea62abd6218b3576
DenisShutov <shtvdenis93@yandex.ru>
2025-10-11 13:36:11 +0500

    Новая игра калькулятор

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,11 @@
+from brain_games.games.brain_calc_game import brain_calc
+from brain_games.scripts.brain_games import greet
+
+
+def main():
+    greet()
+    brain_calc()
+
+
+if __name__ == '__main__':
+    main()
