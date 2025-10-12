9cdc3c28bda4b0c5b46f730346e6d8d8caef146c
DenisShutov <shtvdenis93@yandex.ru>
2025-10-12 17:52:39 +0500

    код игры

---
+++ brain_games/games/brain_progression_game.py
@@ -0,0 +1,25 @@
+from random import randrange
+
+from brain_games.engine import engine_game
+
+rules = 'What number is missing in the progression?'
+
+
+def get_question_answer():
+    start = randrange(1, 11)
+    step = randrange(1, 6)
+    n = 10
+    res = []
+    for i in range(n):
+        a = start + i * step
+        res.append(str(a))
+    
+    index = randrange(0, len(res))
+    correct_answer = res[index]
+    res[index] = '..'
+    question = ' '.join(res)
+    return question, correct_answer
+
+
+def brain_progression():
+    engine_game(rules, get_question_answer)
