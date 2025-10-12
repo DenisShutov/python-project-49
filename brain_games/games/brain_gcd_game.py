4a3e508a39a32e72fa06a7cd2996a05a0fc074a8
DenisShutov <shtvdenis93@yandex.ru>
2025-10-12 16:13:39 +0500

    код игры brain_gcd

---
+++ brain_games/games/brain_gcd_game.py
@@ -0,0 +1,23 @@
+from random import randrange
+
+from brain_games.engine import engine_game
+
+rules = 'Find the greatest common divisor of given numbers.'
+
+
+def get_question_answer():
+    n1 = randrange(1, 51)
+    n2 = randrange(1, 51)
+    question = f'{n1} {n2}'
+
+    a = min(n1, n2)
+    res = []
+    for i in range(1, a + 1):
+        if n1 % i == 0 and n2 % i == 0:
+            res.append(i)
+    correct_answer = max(res)
+    return question, correct_answer
+    
+
+def brain_gcd():
+    engine_game(rules, get_question_answer)
